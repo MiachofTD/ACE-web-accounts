aaa2daed061228f54f323a4abcf0fd3ae390e4f9
Lisa Riggle <julisana@gmail.com>
2017-06-05 18:45:03 -0500

    Enforce HTTPS on all URLs
     Changes to be committed:
    	modified:   app/Http/Middleware/Secure.php
    	modified:   app/Http/routes.php

--- app/Http/Middleware/Secure.php
+++ app/Http/Middleware/Secure.php
@@ -17,7 +17,10 @@ class Secure
     public function handle( $request, Closure $next )
     {
         if ( !$request->secure() ) {
-            return redirect()->secure( $request->getRequestUri() );
+            //Because our testing environment puts the public directory in the URL, so try and remove it.
+            $requestUri = str_replace( $request->getBaseUrl(), '', $request->getRequestUri() );
+
+            return redirect()->secure( $requestUri );
         }
 
         return $next( $request );
