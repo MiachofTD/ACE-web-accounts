4dc487e33d5d6b31c4132e4992ae72dce2038e84
Lisa Riggle <julisana@gmail.com>
2017-06-12 21:34:42 -0500

    Updates to the login pages, enforcing a vertical alignment on all of these pages.
     Changes to be committed:
    	modified:   .env.example
    	modified:   config/layout.php
    	modified:   public/css/app.css
    	modified:   public/css/app.css.map
    	modified:   resources/assets/sass/_layout.scss
    	modified:   resources/views/auth/login.twig
    	modified:   resources/views/auth/passwords/email.twig
    	modified:   resources/views/auth/passwords/reset.twig
    	modified:   resources/views/auth/register.twig

--- config/layout.php
+++ config/layout.php
@@ -3,5 +3,6 @@
 return [
     'background' => [
         'animated' => filter_var( env( 'LAYOUT_ANIMATED_BACKGROUND', true ), FILTER_VALIDATE_BOOLEAN ),
-    ]
+    ],
+    'server_list' => env( 'SERVER_LIST_FILE', '#' )
 ];
